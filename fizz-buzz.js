4a6d82006d390690d820276ec02b5c4674e434c2
tuti <radTuti@live.ca>
2019-03-16 17:18:11 -0400

    step 3b: update source to return Buzz

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,4 +1,4 @@
 // fizz-buzz.js
-const fizzBuzz = (number) => number % 3 === 0 ? 'Fizz' : number;
+const fizzBuzz = (number) => number % 5 === 0 ? 'Buzz' : (number % 3 === 0 ? 'Fizz' : number);
 
 module.exports = fizzBuzz;
