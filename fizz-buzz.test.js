6d93abfd35e5dc6b7621c4edf7007b86a5fc5d1b
tuti <radTuti@live.ca>
2019-03-16 17:11:45 -0400

    step 2c: refactor code
    reuse variables in test code

--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -1,14 +1,17 @@
 // fizz-buzz.test.js
 const fizzBuzz = require('./fizz-buzz');
 
+let expected;
+let actual;
+
 test('returns the number input unchanged if not divisible by 3', () => {
-  const expected = 1;
-  const actual = fizzBuzz(1);
+  expected = 1;
+  actual = fizzBuzz(1);
   expect(actual).toEqual(expected);
 });
 
 test('returns Fizz if number is divible by 3', () => {
-  const expected = 'Fizz';
-  const actual = fizzBuzz(6);
+  expected = 'Fizz';
+  actual = fizzBuzz(6);
   expect(actual).toEqual(expected);
 });
