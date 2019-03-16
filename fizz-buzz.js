76be1bf2f2cd4509a35d35f6e21b8b68a4b8e162
tuti <radTuti@live.ca>
2019-03-16 17:26:30 -0400

    step 4b: update source to return FizzBuzz

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,6 +1,8 @@
 // fizz-buzz.js
 const fizzBuzz = (number) => {
-  if (number % 3 === 0) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'FizzBuzz';
+  } else if (number % 3 === 0) {
     return 'Fizz';
   } else if (number % 5 === 0) {
     return 'Buzz';
