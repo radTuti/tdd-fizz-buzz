d7d84777d166b49be81e838861b8c71c78efb11d
tuti <radTuti@live.ca>
2019-03-16 17:30:55 -0400

    step 4c: refactor code
    - remove duplicate evaluation
    - separate if/else code

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,14 +1,13 @@
 // fizz-buzz.js
 const fizzBuzz = (number) => {
-  if (number % 3 === 0 && number % 5 === 0) {
-    return 'FizzBuzz';
-  } else if (number % 3 === 0) {
-    return 'Fizz';
-  } else if (number % 5 === 0) {
-    return 'Buzz';
-  } else {
-    return number;
+  let output = '';
+  if (number % 3 === 0) {
+    output += 'Fizz';
   }
+  if (number % 5 === 0) {
+    output += 'Buzz';
+  }
+  return output || number;
 };
 
 module.exports = fizzBuzz;
