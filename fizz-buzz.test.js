c2ea7e2ccee450215513eaf2df16f7c66506f83c
tuti <radTuti@live.ca>
2019-03-16 17:02:31 -0400

    step 2a: add test for Fizz
    + update fallback test description

--- fizz-buzz.test.js
+++ fizz-buzz.test.js
@@ -1,8 +1,14 @@
 // fizz-buzz.test.js
 const fizzBuzz = require('./fizz-buzz');
 
-test('returns the number input unchanged', () => {
+test('returns the number input unchanged if not divisible by 3', () => {
   const expected = 1;
   const actual = fizzBuzz(1);
+  expect(actual).toEqual(expected);
+});
+
+test('returns Fizz if number is divible by 3', () => {
+  const expected = 'Fizz';
+  const actual = fizzBuzz(6);
   expect(actual).toEqual(expected);
 });
